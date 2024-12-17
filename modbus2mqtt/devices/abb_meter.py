c519937a00faed5e022cdf35aec6818b0d63095b
Tobias Müller <Tobias_Mueller@twam.info>
2024-12-17 14:48:12 +0100

    Add support for Victron and handle exceptions in ABB

--- modbus2mqtt/devices/abb_meter.py
+++ modbus2mqtt/devices/abb_meter.py
@@ -9,6 +9,7 @@ from types import MappingProxyType
 from construct import Adapter, Byte, Int16sb, Int16ub, Int32sb, Int32ub, Int64sb, Int64ub, PaddedString, Padding, Struct
 
 from modbus2mqtt.devices import Device
+from pymodbus.exceptions import ModbusIOException, ConnectionException
 
 
 class Factor(Adapter):
@@ -223,55 +224,66 @@ class AbbMeter(Device):
         await asyncio.sleep((dt - now).total_seconds())
 
     async def get_messages(self):
-        productdata_and_identification = await self.client.read_holding_registers(
-            address=0x8900, count=self.PRODUCTDATA_AND_IDENTIFICATION.sizeof() // 2, slave=self.unit,
-        )
-        parsed_productdata_and_identification = self.PRODUCTDATA_AND_IDENTIFICATION.parse(
-            bytes(reduce(iadd, [[v >> 8, v & 0xFF] for v in productdata_and_identification.registers], [])),
-        )
-
-        if parsed_productdata_and_identification is None:
-            logging.error("Could not parse 'productdata_and_identification'.")
-            return
-
-        serial_number = parsed_productdata_and_identification.search("SerialNumber")
-
-        for name, topic in self.PRODUCTDATA_AND_IDENTIFICATION_TOPICS.items():
-            for parsed_data in [parsed_productdata_and_identification]:
-                value = parsed_data.search(rf"^{name}$")
-                if value is not None:
-                    yield {'topic': f"{serial_number}/{topic}", 'payload': value, 'retain': True}
-
-        next_send = {}
-
-        while True:
-            now = datetime.now(tz=UTC).timestamp()
-
-            energy_total = await self.client.read_holding_registers(address=0x5000, count=self.ENERGY_TOTAL.sizeof() // 2, slave=self.unit)
-            parsed_energy_total = self.ENERGY_TOTAL.parse(bytes(reduce(iadd, [[v >> 8, v & 0xFF] for v in energy_total.registers], [])))
-
-            energy_per_phase = await self.client.read_holding_registers(
-                address=0x5460, count=self.ENERGY_PER_PHASE.sizeof() // 2, slave=self.unit,
+        try:
+            productdata_and_identification = await self.client.read_holding_registers(
+                address=0x8900, count=self.PRODUCTDATA_AND_IDENTIFICATION.sizeof() // 2, slave=self.unit,
             )
-            parsed_energy_per_phase = self.ENERGY_PER_PHASE.parse(bytes(reduce(iadd, [[v >> 8, v & 0xFF] for v in energy_per_phase.registers], [])))
+            parsed_productdata_and_identification = self.PRODUCTDATA_AND_IDENTIFICATION.parse(
+                bytes(reduce(iadd, [[v >> 8, v & 0xFF] for v in productdata_and_identification.registers], [])),
+            )
 
-            measurements = await self.client.read_holding_registers(address=0x5B00, count=self.MEASUREMENTS.sizeof() // 2, slave=self.unit)
-            parsed_measurements = self.MEASUREMENTS.parse(bytes(reduce(iadd, [[v >> 8, v & 0xFF] for v in measurements.registers], [])))
+            if parsed_productdata_and_identification is None:
+                logging.error("Could not parse 'productdata_and_identification'.")
+                return
 
-            for name, topic in self.TOPICS.items():
-                for parsed_data in [parsed_energy_total, parsed_energy_per_phase, parsed_measurements]:
+            serial_number = parsed_productdata_and_identification.search("SerialNumber")
+
+            for name, topic in self.PRODUCTDATA_AND_IDENTIFICATION_TOPICS.items():
+                for parsed_data in [parsed_productdata_and_identification]:
                     value = parsed_data.search(rf"^{name}$")
                     if value is not None:
-                        interval = 5
-                        for topic_regex, topic_interval in self.config.get("intervals", {}).items():
-                            if re.match(topic_regex, topic):
-                                interval = topic_interval
-                                break
+                        yield {'topic': f"{serial_number}/{topic}", 'payload': value, 'retain': True}
 
-                        if now > next_send.get(topic, 0):
-                            next_send[topic] = (now // interval + 1) * interval
-                            yield {'topic': f"{serial_number}/{topic}", 'payload': value}
+            logging.info(f"Found ABB {parsed_productdata_and_identification['TypeDesignation']} with serial number {serial_number} at {self.client.ctx.comm_params.host}:{self.client.ctx.comm_params.port} on unit {self.unit}.")
 
-            next_wakeup = min(next_send.values())
+            next_send = {}
 
-            await asyncio.sleep(next_wakeup - datetime.now(tz=UTC).timestamp())
+            while True:
+                now = datetime.now(tz=UTC).timestamp()
+
+                energy_total = await self.client.read_holding_registers(address=0x5000, count=self.ENERGY_TOTAL.sizeof() // 2, slave=self.unit)
+                parsed_energy_total = self.ENERGY_TOTAL.parse(bytes(reduce(iadd, [[v >> 8, v & 0xFF] for v in energy_total.registers], [])))
+
+                # if self.client.ctx.comm_params.host == 'waveshare-waschkueche':
+                #     logging.info(f"{self.client.ctx.comm_params.host} {self.unit}: {parsed_energy_total['ActiveNet']}")
+
+                energy_per_phase = await self.client.read_holding_registers(
+                    address=0x5460, count=self.ENERGY_PER_PHASE.sizeof() // 2, slave=self.unit,
+                )
+                parsed_energy_per_phase = self.ENERGY_PER_PHASE.parse(bytes(reduce(iadd, [[v >> 8, v & 0xFF] for v in energy_per_phase.registers], [])))
+
+                measurements = await self.client.read_holding_registers(address=0x5B00, count=self.MEASUREMENTS.sizeof() // 2, slave=self.unit)
+                parsed_measurements = self.MEASUREMENTS.parse(bytes(reduce(iadd, [[v >> 8, v & 0xFF] for v in measurements.registers], [])))
+
+                for name, topic in self.TOPICS.items():
+                    for parsed_data in [parsed_energy_total, parsed_energy_per_phase, parsed_measurements]:
+                        value = parsed_data.search(rf"^{name}$")
+                        if value is not None:
+                            interval = 5
+                            for topic_regex, topic_interval in self.config.get("intervals", {}).items():
+                                if re.match(topic_regex, topic):
+                                    interval = topic_interval
+                                    break
+
+                            if now > next_send.get(topic, 0):
+                                next_send[topic] = (now // interval + 1) * interval
+                                yield {'topic': f"{serial_number}/{topic}", 'payload': value}
+
+                next_wakeup = min(next_send.values())
+
+                await asyncio.sleep(next_wakeup - datetime.now(tz=UTC).timestamp())
+        except ModbusIOException as e:
+            logging.error(f"{self.client.ctx.comm_params.host}:{self.client.ctx.comm_params.port} unit {self.unit} failed with: {e.message}")
+        except ConnectionException as e:
+            logging.error(f"Connection to {self.client.ctx.comm_params.host}:{self.client.ctx.comm_params.port} unit {self.unit} failed. Retrying in 5 s.")
+            await asyncio.sleep(5)
---
+++ modbus2mqtt/devices/victron.py
@@ -0,0 +1,117 @@
+import asyncio
+import logging
+import re
+from datetime import UTC, datetime
+from enum import Enum, auto
+from functools import reduce
+from operator import iadd
+from types import MappingProxyType
+from collections import namedtuple
+
+
+from construct import Adapter, Byte, Int16sb, Int16ub, Int32sb, Int32ub, Int64sb, Int64ub, PaddedString, Padding, Struct
+
+from modbus2mqtt.devices import Device
+from modbus2mqtt.exceptions import InvalidConfigurationError
+from modbus2mqtt.construct_types import Factor
+
+
+RegisterSet = namedtuple('RegisterSet', ['start_address', 'registers'])
+VariantData = namedtuple('VariantData', ['register_sets', 'topics'])
+
+
+class Variant(Enum):
+    SYSTEM = auto()
+    BATTERY = auto()
+    VEBUS = auto()
+
+
+class Victron(Device):
+
+    VARIANT_DATA = {
+        Variant.SYSTEM: VariantData(
+            register_sets=[RegisterSet(start_address=800, registers=Struct(
+                    "Serial" / PaddedString(12, encoding="ASCII"),
+                    # "RelayState1" / Int16ub,
+                    # "RelayState2" / Int16ub,
+                    Padding((843-806)*2),
+                    # "Soc" / Int16ub ,       
+                ))],
+            topics={
+                }
+            ),
+        Variant.BATTERY: VariantData(
+            register_sets=[RegisterSet(start_address=258, registers=Struct(
+                "BatteryPower" / Factor(1, Int16sb),
+                "BatteryVoltage" / Factor(0.01, Int16ub),
+                Padding(2),
+                "BatteryCurrent" / Factor(0.1, Int16sb),
+                "BatteryTemperature" / Factor(0.1, Int16sb),
+                Padding(6),
+                "BatteryStateOfCharge" / Factor(0.1, Int16sb),
+            )),
+            RegisterSet(307, Struct(
+                "BatteryMaxChargeCurrent" / Factor(0.1, Int16ub),
+                "BatteryMaxDischargeCurrent" / Factor(0.1, Int16ub),
+            ))],
+            topics={
+                "BatteryPower": "battery/power",
+                "BatteryVoltage": "battery/voltage",
+                "BatteryCurrent": "battery/current",
+                "BatteryTemperature": "battery/temperature",
+                "BatteryStateOfCharge": "battery/stateofcharge",
+                "BatteryMaxChargeCurrent": "battery/maxchargecurrent",
+                "BatteryMaxDischargeCurrent": "battery/maxdischargecurrent",
+                }
+            ),
+        Variant.VEBUS: [
+        ]
+    }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if 'variant' not in kwargs['config']:
+            raise InvalidConfigurationError(f'Variant not specified for unit {self.unit}.')
+
+        try:
+            self.variant = Variant[kwargs['config']['variant'].upper()]
+        except KeyError:
+            raise InvalidConfigurationError(f'Variant {kwargs['config']['variant']} specified for unit {self.unit} is not supported.')
+
+    # @staticmethod
+    # async def _wait_until(dt: datetime):
+    #     now = datetime.now(tz=UTC)
+    #     await asyncio.sleep((dt - now).total_seconds())
+
+    async def get_messages(self):
+        register_sets = self.VARIANT_DATA[self.variant].register_sets
+        topics = self.VARIANT_DATA[self.variant].topics
+
+        while True:
+            now = datetime.now(tz=UTC).timestamp()
+
+            try:
+                parsed_data_list = []
+                for register_set in register_sets:
+                    data = await self.client.read_holding_registers(
+                        address=register_set.start_address, count=register_set.registers.sizeof() // 2, slave=self.unit,
+                    )
+                    parsed_data_list.append(register_set.registers.parse(
+                        bytes(reduce(iadd, [[v >> 8, v & 0xFF] for v in data.registers], []))
+                    ))
+
+                for name, topic in topics.items():
+                    for parsed_data in parsed_data_list:
+                        value = parsed_data.search(rf"^{name}$")
+                        if value is not None:
+                            yield {'topic': f"{topic}", 'payload': value}
+            except Exception as e :
+                logging.error(f"Reading data from Variant {self.variant.name} for unit {self.unit} failed: {e}")
+
+
+            next_wakeup = now + 1
+            await asyncio.sleep(next_wakeup - datetime.now(tz=UTC).timestamp())
+
+        return
+
